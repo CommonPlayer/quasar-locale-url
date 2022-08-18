1b274c8bc091ca3e3eb9bee96d53a8519357e172
Jimmy <thedd5d2092d50@yahoo.com.hk>
2022-08-18 14:13:22 +0800

    supporting components

---
+++ src/components/LocaleLink.vue
@@ -0,0 +1,12 @@
+<template>
+<router-link :to="`/${$i18n.locale}${to}`"><slot></slot></router-link>
+</template>
+
+<script setup>
+import { toRefs } from 'vue';
+
+const props = defineProps({
+  to: String,
+});
+const { to } = toRefs(props);
+</script>
---
+++ src/components/LocaleSelector.vue
@@ -0,0 +1,22 @@
+<template>
+<router-link
+  v-for="locale in $i18n.availableLocales"
+  :key="'locale_' + locale"
+  :to="route.fullPath.replace(/^\/[\w-]+/, '/' + locale)"
+  class="q-ml-sm"
+>
+  {{ locale }}
+</router-link>
+</template>
+
+<script setup>
+import { watch } from 'vue';
+import { useI18n } from 'vue-i18n';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const i18n = useI18n();
+watch(() => route.fullPath, () => {
+  i18n.locale.value = route.params.locale;
+});
+</script>
